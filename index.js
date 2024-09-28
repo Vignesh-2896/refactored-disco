7c9aef9738070fd37d6f10d500124fad780fc97d
Vignesh <vigneshb2896@gmail.com>
2024-09-28 19:27:25 +0530

    Formatting and Corrections.

--- index.js
+++ index.js
@@ -1,81 +1,89 @@
 const container = document.querySelector("#container");
-var color_type = "yellow";
+let color_type = "yellow";
 function createGrids(size) {
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let gridArea = size * size;
 
-    let gridArea = size * size;
-
-    for(let i=1;i<=gridArea;i++){
-        let div = document.createElement("div");
-        div.setAttribute("id","grid");
-        div.addEventListener("mouseover",change_color);
-        container.appendChild(div);
-    }
-};
-
-function reset(){
-    while(container.firstChild){ container.removeChild(container.firstChild)};
-    new_size = prompt("Enter new size of grids");
-    createGrids(new_size);
-};
-
-function erase(){
-    let gridItems = document.querySelectorAll("#grid");
-    gridItems.forEach(gridItems => gridItems.style.backgroundColor = "");
+  for (let i = 1; i <= gridArea; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("id", "grid");
+    div.addEventListener("mouseover", change_color);
+    container.appendChild(div);
+  }
 }
 
-function change_color(){
-
-    switch(color_type){
-        case 'random':          
-            let red = Math.floor(Math.random() * 256);
-            let green = Math.floor(Math.random() * 256);
-            let blue = Math.floor(Math.random() * 256);       
-            this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; 
-            break;
-
-        case 'yellow':
-            this.style.backgroundColor = "yellow"
-            break;
-
-        case 'rainbow':
-            colorList = ['#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#4b0082', '#ee82ee'];
-            randomColorPos = Math.floor(Math.random() * colorList.length);
-            this.style.backgroundColor = colorList[randomColorPos];
-            break;
-
-        case 'grayscale':
-            colorList = ['#BEBEBE', '#A0A0A0', '#D8D8D8', '#505050'];
-            randomColorPos = Math.floor(Math.random() * colorList.length);
-            this.style.backgroundColor = colorList[randomColorPos];
-            break;
-    }
-
-   
-};
-
-function set_colors(color_option){
-    erase();
-    switch(color_option.value){
-
-        case 'rainbow':
-            color_type = "rainbow";
-            break;
-
-        case 'grayscale':
-            color_type = "grayscale";
-            break;
-
-        case 'random':
-            color_type = "random";
-            break;
-
-        default:
-            color_type = "";
-            break;
-    }
+function reset() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let new_size = prompt("Enter new size of grids");
+  createGrids(new_size);
 }
 
-window.onload = createGrids(10);
+function erase() {
+  let gridItems = document.querySelectorAll("#grid");
+  gridItems.forEach((gridItems) => (gridItems.style.backgroundColor = ""));
+}
+
+function change_color() {
+  switch (color_type) {
+    case "random": {
+      let red = Math.floor(Math.random() * 256);
+      let green = Math.floor(Math.random() * 256);
+      let blue = Math.floor(Math.random() * 256);
+      this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+      break;
+    }
+    case "yellow":
+      this.style.backgroundColor = "yellow";
+      break;
+
+    case "rainbow": {
+      let colorList = [
+        "#ff0000",
+        "#ffa500",
+        "#ffff00",
+        "#008000",
+        "#0000ff",
+        "#4b0082",
+        "#ee82ee",
+      ];
+      let randomColorPos = Math.floor(Math.random() * colorList.length);
+      this.style.backgroundColor = colorList[randomColorPos];
+      break;
+    }
+    case "grayscale": {
+      let colorList = ["#BEBEBE", "#A0A0A0", "#D8D8D8", "#505050"];
+      let randomColorPos = Math.floor(Math.random() * colorList.length);
+      this.style.backgroundColor = colorList[randomColorPos];
+      break;
+    }
+  }
+}
+
+function set_colors(color_option) {
+  erase();
+  switch (color_option.value) {
+    case "rainbow":
+      color_type = "rainbow";
+      break;
+
+    case "grayscale":
+      color_type = "grayscale";
+      break;
+
+    case "random":
+      color_type = "random";
+      break;
+
+    default:
+      color_type = "";
+      break;
+  }
+}
+
+window.onload = () => {
+  createGrids(10);
+};
